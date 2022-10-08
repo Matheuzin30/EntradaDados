00fd1342a17a435ee33533b9e1769f96eea13328
Matheuzin30 <mat860913@gmail.com>
2022-10-08 11:34:21 -0300

    Adicionar arquivos de projeto.

---
+++ EntradaDados/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+namespace EstruturaDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome Completo");
+            string fullname = Console.ReadLine();
+            Console.WriteLine("Quantos Quartos Tem em sua Casa?");
+            int bedrooms = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre seu último nome, idade e altura (mesma linha):");
+            double price = double.Parse(s: Console.ReadLine(), CultureInfo.InvariantCulture);
+            string[] vect = Console.ReadLine().Split(' ');
+            string lastName = vect[0];
+            int age = int.Parse(vect[1]);
+            double height = double.Parse(vect[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(fullname);
+            Console.WriteLine(bedrooms);
+            Console.WriteLine(price.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(lastName);
+            Console.WriteLine(age);
+            Console.WriteLine(height.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
